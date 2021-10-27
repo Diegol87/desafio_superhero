b123fd4e2066c92aced847729c897c37f4de000f
Diego <da.infanta@gmail.com>
2021-10-27 13:19:22 -0300

    primera version

---
+++ assets/js/scripts.js
@@ -0,0 +1,123 @@
+
+
+$(document).ready(function() {
+
+    $(".btn").click(function(e) {
+
+    e.preventDefault();
+
+    let numero1 = parseInt(document.querySelector("#numeros").value);
+
+    let resultado = validar(numero1);
+
+    if(resultado === true) {
+        exito();
+    }
+
+    function exito() {    
+
+        let numero1 = parseInt(document.querySelector("#numeros").value);
+        let resultado = numero1;
+
+        $.ajax({
+            type: "GET",
+            url:"https://superheroapi.com/api.php/921606152096118/" + resultado,
+            dataType:"json",
+            success: function(datosHero) {
+
+                $(".card-title").text("Nombre: " + datosHero.name);
+                $("#card1").text("Conexiones: " + datosHero.connections["group-affiliation"]);
+                $("#card2").text("Publicado por: " + datosHero.biography.publisher);
+                $("#card3").text("Ocupación: " + datosHero.work.occupation);
+                $("#card4").text("Primera aparición: " + datosHero.biography["first-appearance"]);
+                $("#card5").text("Altura: " + datosHero.appearance.height);
+                $("#card6").text("Peso: " + datosHero.appearance.weight);
+                $("#card7").text("Alianzas: " + datosHero.biography.aliases);
+                $(".img-fluid").attr("src", datosHero.image.url);
+            },
+        }); 
+    }   
+    });
+});
+
+// Restricciones al momento de ingresar la solicitud en la búsqueda
+
+    function validar(numero1) {
+        let pasamosLaValidacion = true;
+        let patron = /\d/gi;
+
+        if(numero1 === "") {
+            document.querySelector(".error").innerHTML = "Debes ingresar un número entre el 1 y el 732, incluído ambos"
+
+            pasamosLaValidacion = false;
+
+        } else if(patron.test(numero1) === false) {
+            document.querySelector(".error").innerHTML = "Formato incorrecto, debes ingresar un número entre el 1 y el 732, incluído ambos"
+            document.querySelector(".card").style.display = "none";
+
+            pasamosLaValidacion = false;
+
+        } else if(numero1 > 732 || numero1 < 1) {
+            document.querySelector(".error").innerHTML = " Rango incorrecto, debes ingresar un número entre el 1 y el 732, incluído ambos"
+            document.querySelector(".card").style.display = "none";
+
+            pasamosLaValidacion = false;
+
+        } else {
+            document.querySelector(".error").innerHTML = " "
+            document.querySelector(".card").style.display = "block";
+
+            pasamosLaValidacion = true;
+        }
+        return pasamosLaValidacion
+    }
+
+   //Gráfico
+
+    $(".btn").click(function() {
+
+        let numero2 = parseInt(document.querySelector("#numeros").value);
+        let resultado1 = numero2;
+
+        let dataPoints = [];
+        let opciones = {
+            title: {
+                text: "Estadística de habilidades "
+            },
+            data: [{
+                    type: "pie",
+                    startAngle: 45,
+                    showInLegend: "true",
+                    legendText: "{label}",
+                    indexLabel: "{label} ({y})",
+                    yValueFormatString:"#,##0.#"%"",
+                    dataPoints: dataPoints,
+        },
+    ],
+    }
+
+        $.ajax({
+            type: "GET",
+            url:"https://superheroapi.com/api.php/921606152096118/" + resultado1,
+            dataType:"json",
+            success: function(data) {
+                let datosApi = [data.powerstats.combat, data.powerstats.durability, data.powerstats.intelligence, data.powerstats.power, data.powerstats.speed, data.powerstats.strength];
+
+                let datosLabel = ["Combat", "Durability", "Intelligence", "Power", "Speed", "Strength"]
+                
+                for(let i = 0; i < datosApi.length ; i++) {
+                    dataPoints.push({
+                    y: datosApi[i],
+                    label: datosLabel[i]
+                    });
+            }
+
+            $("#chartContainer").CanvasJSChart(opciones);
+            
+        },
+        error: function(error) {
+            console.log(error);
+          },
+        });
+    });
+    
